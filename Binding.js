2f3a62e8e064b21830ef93b25e5f84fd05160039
David Brown <dcbrown07@gmail.com>
2015-09-01 23:58:34 -0400

    init data-binding

---
+++ Binding.js
@@ -0,0 +1,54 @@
+export class Binding {
+  constructor(data) {
+    this.data = data
+    this.findBindable()
+
+    if (Object.observe) {
+
+      Object.observe(this.data, (changes) => {
+        changes.forEach((change) => {
+          let bindings = document.querySelectorAll('[liaison-bind=' + change.name + ']')
+          for (let j = 0; j < bindings.length; j++) {
+            this.setElementContent(bindings[j], change.object[change.name])
+          }
+        });
+      });
+      this.populateBindings()
+    } else {
+      var currentData = currentData || {}
+      setInterval(() => {
+        if (JSON.stringify(currentData) !== JSON.stringify(this.data)) {
+          currentData = JSON.parse(JSON.stringify(this.data));
+          this.populateBindings()
+        }
+      }, 100)
+    }
+  }
+
+  populateBindings() {
+    let elms = document.querySelectorAll('[liaison-bind]')
+    for (let i = 0, l = elms.length; i < l; i++) {
+      this.setElementContent(elms[i], this.data[elms[i].getAttribute('liaison-bind')])
+    }
+  }
+
+  findBindable() {
+    var me = this;
+    let elms = document.querySelectorAll('[liaison-bind]')
+    for (let i = 0, l = elms.length; i < l; i++) {
+      elms[i].addEventListener('keyup', function(e) {
+        let newVal = e.currentTarget.value;
+        me.data[this.getAttribute('liaison-bind')] = newVal
+        let bindings = document.querySelectorAll('[liaison-bind=' + this.getAttribute('liaison-bind') + ']')
+        for (let j = 0; j < bindings.length; j++) {
+          me.setElementContent(bindings[j], newVal)
+        }
+      })
+    }
+  }
+
+  setElementContent(element, value) {
+    element.innerHTML = value
+    element.value = value
+  }
+}
