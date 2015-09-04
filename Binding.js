be315ae2646ae0548a9ed0d872ce9bc331e02a82
David Brown <dcbrown07@gmail.com>
2015-09-04 17:08:49 -0400

    updating for loops

--- Binding.js
+++ Binding.js
@@ -30,12 +30,14 @@ export class Binding {
 
   populateBindings() {
     let elms = document.querySelectorAll('[liaison-bind]')
+    // TODO clean this up. its ugly and a nested loop
     for (let i = 0, l = elms.length; i < l; i++) {
       let el = elms[i].getAttribute('liaison-bind');
       //if nested
       if (el.indexOf('.') > 0) {
         let exploded = el.split('.')
         let temp = this.data[exploded[0]];
+
         for (let j = 1; j < exploded.length; j++) {
           this.setElementContent(elms[i], temp[exploded[j]])
           temp = temp[exploded[j]]
@@ -49,21 +51,27 @@ export class Binding {
   findBindable() {
     var me = this;
     let elms = document.querySelectorAll('[liaison-bind]')
-    for (let i = 0, l = elms.length; i < l; i++) {
-      elms[i].addEventListener('keyup', function(e) {
+
+    // TODO get this working without having to run slice
+    elms = Array.prototype.slice.call(elms, 0)
+
+    for (let elm of elms) {
+      elm.addEventListener('keyup', function(e) {
         let newVal = e.currentTarget.value;
 
         //deep update
-        let el = elms[i].getAttribute('liaison-bind');
+        let el = elm.getAttribute('liaison-bind');
         if (el.indexOf('.') > 0) {
           //at deepest object set it to newVal
-          debugger
           me.setValue(me.data, el, newVal);
         } else {
           me.data[el] = newVal
           let bindings = document.querySelectorAll('[liaison-bind="' + this.getAttribute('liaison-bind') + '"]')
-          for (let j = 0; j < bindings.length; j++) {
-            me.setElementContent(bindings[j], newVal)
+
+          // TODO get this working without having to run slice
+          bindings = Array.prototype.slice.call(bindings, 0)
+          for (let binds of bindings) {
+            me.setElementContent(bind, newVal)
           }
         }
 
