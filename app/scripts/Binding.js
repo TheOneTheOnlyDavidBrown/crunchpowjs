b3d99d3cb2e5f742bc741ea243afdda0e2de365a
David Brown <david@davidbrownucf.com>
2015-09-05 00:16:10 -0400

    cleanup

--- app/scripts/Binding.js
+++ app/scripts/Binding.js
@@ -15,21 +15,11 @@ export class Binding {
 
   populateBindings() {
     let elms = document.querySelectorAll('[liaison-bind]')
-    // TODO clean this up. its ugly and a nested loop
-    for (let i = 0, l = elms.length; i < l; i++) {
-      let el = elms[i].getAttribute('liaison-bind');
-      //if nested
-      if (el.indexOf('.') > 0) {
-        let exploded = el.split('.')
-        let temp = this.data[exploded[0]];
+    // TODO get this working without having to run slice
+    elms = Array.prototype.slice.call(elms, 0)
 
-        for (let j = 1; j < exploded.length; j++) {
-          this.setElementContent(elms[i], temp[exploded[j]])
-          temp = temp[exploded[j]]
-        }
-      } else {
-        this.setElementContent(elms[i], this.data[el])
-      }
+    for (let elm of elms){
+      this.setElementContent(elm, this.getValue(this.data, elm.getAttribute('liaison-bind')))
     }
   }
 
@@ -40,21 +30,14 @@ export class Binding {
     elms = Array.prototype.slice.call(elms, 0)
 
     for (let elm of elms) {
-      elm.addEventListener('keyup', (e) => {
-        let newVal = e.currentTarget.value;
-
-        //deep update
-        let elms = elm.getAttribute('liaison-bind');
-        //at deepest object set it to newVal
-        this.setValue(this.data, elms, newVal);
-      })
+      elm.addEventListener('keyup', (e) => this.setValue(this.data, elm.getAttribute('liaison-bind'), e.currentTarget.value))
     }
   }
 
   setElementContent(element, value) {
-      element.innerHTML = value
-      element.value = value
-    }
+    element.innerHTML = value
+    element.value = value
+  }
     /**
      * @obj: the json object to change
      * @access: string dot separates route to value
@@ -71,4 +54,14 @@ export class Binding {
     }
   }
 
+  getValue(obj, access) {
+    if (typeof(access) == 'string') {
+      access = access.split('.');
+    }
+    if (access.length > 1) {
+      return this.getValue(obj[access.shift()], access);
+    } else {
+      return obj[access[0]]
+    }
+  }
 }
