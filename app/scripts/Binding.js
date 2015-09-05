30c42cb592c51f047d15373e00d280b31daa1d0f
David Brown <david@davidbrownucf.com>
2015-09-05 12:02:10 -0400

    performance fix

--- app/scripts/Binding.js
+++ app/scripts/Binding.js
@@ -8,7 +8,9 @@ export class Binding {
 
     var currentData = currentData || {}
 
+    // watching this.data for programmatical updates
     setInterval(() => {
+      // TODO find a better way to compare
       if (JSON.stringify(currentData) !== JSON.stringify(this.data)) {
         currentData = JSON.parse(JSON.stringify(this.data))
         this.populateBindings()
@@ -18,13 +20,21 @@ export class Binding {
 
   populateBindings() {
     for (let elm of document.querySelectorAll('[liaison-bind]')){
-      this.setElementContent(elm, this.getValue(this.data, elm.getAttribute('liaison-bind')))
+      let bindName = document.activeElement.getAttribute('liaison-bind')
+
+      //prevent the input youre typing in from updating. prevents the cursor from jumping to the end
+      //only updates bindings that need to be updated
+      if (elm !== document.activeElement && (elm.getAttribute('liaison-bind') === bindName || bindName === null)){
+        this.setElementContent(elm, this.getValue(this.data, elm.getAttribute('liaison-bind')))
+      }
     }
   }
 
   findBindable() {
     for (let elm of document.querySelectorAll('[liaison-bind]')){
-      elm.addEventListener('keyup', (e) => this.setValue(this.data, elm.getAttribute('liaison-bind'), e.currentTarget.value))
+      elm.addEventListener('keyup', (e) => {
+        this.setValue(this.data, elm.getAttribute('liaison-bind'), e.currentTarget.value)
+      })
     }
   }
 
@@ -32,14 +42,10 @@ export class Binding {
     element.innerHTML = value
     element.value = value
   }
-    /**
-     * @obj: the json object to change
-     * @access: string dot separates route to value
-     * @value: new valu
-     */
+
   setValue(obj, access, value) {
     if (typeof(access) == 'string') {
-      access = access.split('.');
+      access = access.split('.')
     }
     if (access.length > 1) {
       this.setValue(obj[access.shift()], access, value);
@@ -50,7 +56,7 @@ export class Binding {
 
   getValue(obj, access) {
     if (typeof(access) == 'string') {
-      access = access.split('.');
+      access = access.split('.')
     }
     if (access.length > 1) {
       return this.getValue(obj[access.shift()], access);
