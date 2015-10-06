4b0d1cca53dc4f1803bbcf691fe308addcb0afde
David Brown <dcbrown07@gmail.com>
2015-10-06 16:23:20 -0400

    refactor to utils

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,7 @@
+import Utils from './Utils'
 export default class Router {
   constructor() {
+    this.utils = new Utils();
     this.view = document.querySelector('[liaison-view]');
     this.paths = [];
     this.wildcards = [];
@@ -40,13 +42,10 @@ export default class Router {
   }
 
   // TODO: clean this up. there has to be a better way
-  // TODO: write unit test for this. integration test is good but this can be unit tested which yields more consistent results
   findRouteInPaths(route) {
     for (let path of this.paths) {
       if (path.name === route) return path;
     }
-    console.log('not found natural path. searching wildcards');
-
     route = route.substring(1).split('/');
     for (let wildcard of this.wildcards) {
       let _wildcard = wildcard.name.substring(1).split('/');
@@ -55,27 +54,14 @@ export default class Router {
           if (_wildcard[i].indexOf(':') === 0) {
             let temp = _wildcard;
             temp[i] = route[i];
-            if (this.isIdentical(temp, route)) {
+            if (this.utils.compareByConvert(temp, route)) {
               return wildcard
             }
           }
         }
       }
     }
-    console.log('going to fallback')
     return false;
-  }
-
-  // TODO: put this in a helper library
-  // this function was found at http://stackoverflow.com/questions/7837456/comparing-two-arrays-in-javascript
-  // this is a shallow check
-  isIdentical(a, b) {
-    var i = a.length;
-    if (i != b.length) return false;
-    while (i--) {
-      if (a[i] !== b[i]) return false;
-    }
-    return true;
   }
 }
 
